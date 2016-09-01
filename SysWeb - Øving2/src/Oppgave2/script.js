7133d25db39e389ed44c5faa8f2bbbfe613b2d40
Bits Please <olavhus@student.hist.no>
2016-09-01 15:17:42 +0200

    removed doubleclick because it was useless

--- SysWeb - Øving2/src/Oppgave2/script.js
+++ SysWeb - Øving2/src/Oppgave2/script.js
@@ -6,17 +6,8 @@
 
 var tiles = document.getElementsByClassName("tabellrute");
 
-function reset() {
 
-  for(i=0; i<tiles.length; i++){
 
-      tiles[i].style = "";
-
-  }
-
-}
-
-var clicks = 0;
 var turn = 0; // 0 = first, 1 = second
 console.log("tiles: "+tiles.length);
 
@@ -29,33 +20,36 @@ for(i=0; i<tiles.length; i++){
     var width = tile.width;
 
     tile.onmousedown = function () {
-        clicks++;
 
         if (this.className == "playerOne" || this.className == "playerTwo"){
 
             if ( (this.className == "playerOne" && turn==0) || (this.className == "playerTwo" && turn==1)){
-                clicks = 0;
                 return;
             }
 
             turn = turn ==0 ? 1 : 0;
-            clicks = 0;
             this.className = "tabellrute";
             return;
         }
 
-        if (clicks == 1 && turn == 0 && this.className != "playerTwo") {
+        if ( turn == 0 && this.className != "playerTwo") {
             this.className = "playerOne";
-            clicks = 0;
             turn = 1;
         }
-        else if (clicks == 2 && turn == 1 && this.className != "playerOne"){
+        else if ( turn == 1 && this.className != "playerOne"){
             this.className = "playerTwo";
 
-            clicks = 0;
             turn = 0;
         }
         else if (clicks >= 2){clicks = 0;}
 
+    }
+}
+
+function reset() {
+
+    for(i=0; i<tiles.length; i++){
+
+        tiles[i].style = "";
     }
 }
